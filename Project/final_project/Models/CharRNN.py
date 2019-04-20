330ed3b72bb50bbee8e10176b4bc83e7980704bc
chong guo <chongguo.hms@gmail.com>
2019-04-20 11:38:11 -0400

    project update

---
+++ Project/final_project/Models/CharRNN.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CharRNN(nn.Module):
+    def __init__(self, n_inputs, n_hidden,n_output,n_layers,model,device):
+        super(CharRNN, self).__init__()
+        self.n_inputs = n_inputs
+        self.n_hidden = n_hidden
+        self.n_output = n_output
+        self.n_layers = n_layers
+        self.model = model
+        self.encoder = nn.Linear(n_inputs, n_hidden)
+        self.decoder = nn.Linear(n_hidden, n_output)
+        if self.model == "gru":
+            self.rnn = nn.GRU(n_hidden, n_hidden, n_layers)
+        elif self.model == "lstm":
+            self.rnn = nn.LSTM(n_hidden, n_hidden, n_layers)
+        self.device = device
+        
+    def forward(self, x):
+        x=x.permute(1,0,2)
+        hidden = torch.zeros(self.n_layers,x.shape[1],self.n_hidden).to(self.device)
+        output = torch.zeros(x.shape[0],x.shape[1],self.n_output).to(self.device)
+        output, hidden = self.rnn(self.encoder(x),hidden)
+        output = self.decoder(output)
+        
+        return output, hidden
