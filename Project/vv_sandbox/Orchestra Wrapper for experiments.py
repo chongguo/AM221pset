c769c2b03242f9415ba8d337db2d589252c2b1bc
Vinay Viswanadham <vinayak.vsv@gmail.com>
2019-04-21 16:21:20 -0400

    mlp experiments 4/21

--- Project/vv_sandbox/Orchestra Wrapper for experiments.py
+++ Project/vv_sandbox/Orchestra Wrapper for experiments.py
@@ -5,4 +5,11 @@
 # This orchestra wrapper:
 # * Takes model parameters as input
 # * Takes a dataset as input (in this case, the MNIST dataset is default
-# * Sends multiple instances of the regularization  
+
+# In[ ]:
+
+
+# pseudocode for main
+def main ():
+    return()
+
